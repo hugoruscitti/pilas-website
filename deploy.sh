a482dcf6918f6915e4c42fc78862d13012251de8
Hugo Ruscitti <hugoruscitti@gmail.com>
2015-02-01 01:17:33 -0300

    arreglo menor.

--- deploy.sh
+++ deploy.sh
@@ -8,10 +8,11 @@ git pull
 rm -rf dist
 ember build --environment production
 
-git checkout gh-pages
-git pull origin gh-pages
 mv node_modules ../node_modules_tmp
 mv bower_components ../bower_components_tmp
+
+git checkout gh-pages
+git pull origin gh-pages
 ls -1 | grep -v -E '^dist$' | xargs rm -rf
 mv dist/* ./
 rmdir dist
