50ab096820a233b8c1780ef9b44177b868f47a9e
Hugo Ruscitti <hugoruscitti@gmail.com>
2015-02-12 11:21:13 -0300

    actualizacion...

--- app/routes/blog.js
+++ app/routes/blog.js
@@ -3,8 +3,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    var url = "http://pilas-engine.tumblr.com/api/read/json?callback=c&num=5";
-
-    return Ember.$.getJSON(url).then(function(response) {console.log(response); return response;});
+    return [];
   }
 });
