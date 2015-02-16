b4e3ad04f64931e7ba642e25e0ce610b5611b124
Hugo Ruscitti <hugoruscitti@gmail.com>
2015-02-16 00:07:26 -0300

    Complentando las secciones videos, descargas y documentación.

--- app/routes/descargas.js
+++ app/routes/descargas.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function() {
+    return {
+      version: "0.90.10",
+      fecha: "Marzo del 2015",
+      link_windows: "https://www.dropbox.com/s/wxrnwmzc99rs3eq/pilas-engine_0.90.24.exe?dl=0",
+      link_mac: "https://www.dropbox.com/s/2zvuvnhe8ojqbm1/pilas-engine-0.90.24.dmg?dl=0",
+      link_deb: "http://repo.huayra.conectarigualdad.gob.ar/huayra/pool/main/p/python-pilas/python-pilas_0.90.23_all.deb",
+    };
+  }
 });
--- app/services/videos.js
+++ app/services/videos.js
@@ -1,8 +1,10 @@
 import Ember from 'ember';
 
 var videos = [
-  {id: 1, titulo: "Pilas en PyCon España 2013 (Madrid)", tipo: 'youtube', url: 'http://www.youtube.com/embed/bjlWZjTZLmQ'},
-  {id: 2, titulo: "Pilas en PyCon Argentina 2013 (Rosario)", tipo: 'youtube', url: 'http://www.youtube.com/embed/tXA2BgzrvzA'},
+  {id: 7, titulo: 'Pilas en tv pública', tipo: 'youtube', url: 'http://www.youtube.com/embed/4KpLctdBrf8'},
+  {id: 8, titulo: 'Pilas en WISIT 2014 (Argentina)', tipo: 'youtube', url: 'http://www.youtube.com/embed/l2nh3YYW29k'},
+  {id: 1, titulo: "Pilas en PyCon España 2013 (Madrid, España)", tipo: 'youtube', url: 'http://www.youtube.com/embed/bjlWZjTZLmQ'},
+  {id: 2, titulo: "Pilas en PyCon Argentina 2013 (Rosario, Argentina)", tipo: 'youtube', url: 'http://www.youtube.com/embed/tXA2BgzrvzA'},
   {id: 3, titulo: "Pilas en PyCon Argentina 2012", tipo: 'youtube', url: 'http://www.youtube.com/embed/sQhxjLoJlZs'},
   {id: 4, titulo: "Presentación de pilas en betabeers Bs. As. Marzo 2012", tipo: 'youtube', url: 'http://www.youtube.com/embed/-Z6Qi_B9QSA'},
   {id: 5, titulo: "Haciendo videojuegos con pilas (pyday 2011)", tipo: 'vimeo', url: 'http://player.vimeo.com/video/23735704?title=0&amp;byline=0&amp;portrait=0'},
@@ -14,7 +16,7 @@ export default Ember.Object.extend({
     var record = null;
 
     videos.forEach(function(e) {
-      if (e.id == id) {
+      if (e.id === id) {
         record = e;
       }
     });
