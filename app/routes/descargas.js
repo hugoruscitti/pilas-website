93c0014998010c92eda74dc54f9cf5f2e861c725
Hugo Ruscitti <hugoruscitti@gmail.com>
2015-02-17 02:54:15 -0300

    actualizacion...

--- app/routes/descargas.js
+++ app/routes/descargas.js
@@ -5,8 +5,8 @@ export default Ember.Route.extend({
     return {
       version: "0.90.25",
       fecha: "2015-02-16",
-      link_windows: "https://dl.dropboxusercontent.com/u/1335422/releases/pilas-engine/0.90.25/pilas-engine-0.90.25.dmg",
-      link_mac: "https://dl.dropboxusercontent.com/u/1335422/releases/pilas-engine/0.90.25/pilas-engine_0.90.25.exe",
+      link_mac: "https://dl.dropboxusercontent.com/u/1335422/releases/pilas-engine/0.90.25/pilas-engine-0.90.25.dmg",
+      link_windows: "https://dl.dropboxusercontent.com/u/1335422/releases/pilas-engine/0.90.25/pilas-engine_0.90.25.exe",
       link_deb: "http://repo.huayra.conectarigualdad.gob.ar/huayra/pool/main/p/python-pilas/",
     };
   }
--- app/routes/descargas_template.js
+++ app/routes/descargas_template.js
@@ -5,8 +5,8 @@ export default Ember.Route.extend({
     return {
       version: "VERSION",
       fecha: "FECHA",
-      link_windows: "https://dl.dropboxusercontent.com/u/1335422/releases/pilas-engine/VERSION/pilas-engine-VERSION.dmg",
-      link_mac: "https://dl.dropboxusercontent.com/u/1335422/releases/pilas-engine/VERSION/pilas-engine_VERSION.exe",
+      link_mac: "https://dl.dropboxusercontent.com/u/1335422/releases/pilas-engine/VERSION/pilas-engine-VERSION.dmg",
+      link_windows: "https://dl.dropboxusercontent.com/u/1335422/releases/pilas-engine/VERSION/pilas-engine_VERSION.exe",
       link_deb: "http://repo.huayra.conectarigualdad.gob.ar/huayra/pool/main/p/python-pilas/",
     };
   }
