621448ded3325801489349c793e9987e3aa976ff
Hugo Ruscitti <hugoruscitti@gmail.com>
2015-06-11 17:22:20 -0300

    actualizacion...

--- app/routes/descargas.js
+++ app/routes/descargas.js
@@ -3,10 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     return {
-      version: "0.90.33",
-      fecha: "2015-05-23",
-      link_mac: "https://dl.dropboxusercontent.com/u/1335422/releases/pilas-engine/0.90.33/pilas-engine-0.90.33.dmg",
-      link_windows: "https://dl.dropboxusercontent.com/u/1335422/releases/pilas-engine/0.90.33/pilas-engine_0.90.33.exe",
+      version: "0.90.36",
+      fecha: "2015-06-08",
+      link_mac: "https://dl.dropboxusercontent.com/u/1335422/releases/pilas-engine/0.90.36/pilas-engine-0.90.36.dmg",
+      link_windows: "https://dl.dropboxusercontent.com/u/1335422/releases/pilas-engine/0.90.36/pilas-engine_0.90.36.exe",
       link_deb: "http://repo.huayra.conectarigualdad.gob.ar/huayra/pool/main/p/python-pilas/",
     };
   }
