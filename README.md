ecf475bbbdeaddbaf2a10bde51dc3a58d6f0a661
Hugo Ruscitti <hugoruscitti@gmail.com>
2015-06-27 19:19:08 -0300

    actualizacion...

--- README.md
+++ README.md
@@ -70,3 +70,5 @@ con emberjs, liquid-fire y buena suerte :)
 
 
 
+
+
--- app/routes/descargas.js
+++ app/routes/descargas.js
@@ -3,10 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     return {
-      version: "0.90.37",
-      fecha: "2015-06-16",
-      link_mac: "https://dl.dropboxusercontent.com/u/1335422/releases/pilas-engine/0.90.37/pilas-engine-0.90.37.dmg",
-      link_windows: "https://dl.dropboxusercontent.com/u/1335422/releases/pilas-engine/0.90.37/pilas-engine_0.90.37.exe",
+      version: "0.90.38",
+      fecha: "2015-06-17",
+      link_mac: "https://dl.dropboxusercontent.com/u/1335422/releases/pilas-engine/0.90.38/pilas-engine-0.90.38.dmg",
+      link_windows: "https://dl.dropboxusercontent.com/u/1335422/releases/pilas-engine/0.90.38/pilas-engine_0.90.38.exe",
       link_deb: "http://repo.huayra.conectarigualdad.gob.ar/huayra/pool/main/p/python-pilas/",
     };
   }
