2e9fd0a40096f518a1483f2a38715385dbad45e6
Hugo Ruscitti <hugoruscitti@gmail.com>
2015-10-18 00:14:59 -0300

    actualizando tutorial.

--- app/components/p-codigo.js
+++ app/components/p-codigo.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'pre',
+  attributeBindings: ['destacar:data-line'],
+  classNameBindings: ['enumerar:line-numbers'],
 
   didInsertElement() {
     Prism.highlightElement(this.$('code')[0]);
