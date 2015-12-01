58dc52d22cf958d0996d77f51c043f89358cb1e7
Hugo Ruscitti <hugoruscitti@gmail.com>
2015-12-01 14:48:35 -0300

    actualizando links de descargas para que ya no utilicen dropbox.

--- app/routes/descargas.js
+++ app/routes/descargas.js
@@ -5,8 +5,8 @@ export default Ember.Route.extend({
     return {
       version: "1.3.2",
       fecha: "2015-11-04",
-      link_mac: "https://dl.dropboxusercontent.com/u/1335422/releases/pilas-engine/1.3.2/pilas-engine-1.3.2.dmg",
-      link_windows: "https://dl.dropboxusercontent.com/u/1335422/releases/pilas-engine/1.3.2/pilas-engine_1.3.2.exe",
+      link_mac: "http://static.pilas-engine.com.ar/pilas-engine/1.3.2/pilas-engine-1.3.2.dmg",
+      link_windows: "http://static.pilas-engine.com.ar/pilas-engine/1.3.2/pilas-engine_1.3.2.exe",
       link_deb: "http://repo.huayra.conectarigualdad.gob.ar/huayra/pool/main/p/python-pilas/",
     };
   }
