2fbeadd2bff6b6c81face9b026ac7ddeab31fe2e
Hugo Ruscitti <hugoruscitti@gmail.com>
2016-07-28 15:37:50 -0300

    Actualizando links de descarga.

--- app/routes/descargas.js
+++ app/routes/descargas.js
@@ -3,10 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     return {
-      version: "1.4.3",
-      fecha: "2016-03-16",
-      link_mac: "http://static.pilas-engine.com.ar/pilas-engine/1.4.3/pilas-engine-1.4.3.dmg",
-      link_windows: "http://static.pilas-engine.com.ar/pilas-engine/1.4.3/pilas-engine_1.4.3.exe",
+      version: "1.4.7",
+      fecha: "2016-07-28",
+      link_mac: "http://static.pilas-engine.com.ar/pilas-engine/1.4.7/pilas-engine-1.4.7.dmg",
+      link_windows: "http://static.pilas-engine.com.ar/pilas-engine/1.4.7/pilas-engine_1.4.7.exe",
       link_deb: "http://repo.huayra.conectarigualdad.gob.ar/huayra/pool/main/p/python-pilas/",
     };
   }
