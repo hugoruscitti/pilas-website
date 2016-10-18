4df68969d7c9065b3e613ccb438781b2319cf818
Hugo Ruscitti <hugoruscitti@gmail.com>
2016-10-18 18:52:31 -0300

    Actualizando links de descargas para la version 1.4.9

--- app/routes/descargas.js
+++ app/routes/descargas.js
@@ -3,10 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     return {
-      version: "1.4.8",
-      fecha: "2016-08-26",
-      link_mac: "http://static.pilas-engine.com.ar/pilas-engine/1.4.8/pilas-engine-1.4.8.dmg",
-      link_windows: "http://static.pilas-engine.com.ar/pilas-engine/1.4.8/pilas-engine_1.4.8.exe",
+      version: "1.4.9",
+      fecha: "2016-10-18",
+      link_mac: "http://static.pilas-engine.com.ar/pilas-engine/1.4.9/pilas-engine-1.4.9.dmg",
+      link_windows: "http://static.pilas-engine.com.ar/pilas-engine/1.4.9/pilas-engine_1.4.9.exe",
       link_deb: "http://repo.huayra.conectarigualdad.gob.ar/huayra/pool/main/p/python-pilas/",
     };
   }
